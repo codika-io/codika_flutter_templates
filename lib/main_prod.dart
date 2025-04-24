cf2678c87e35c6b84d4d3f3c53da2b61bf5bf2ce
Romain Pattyn <romain.pattyn.ent@gmail.com>
2025-04-24 09:50:00 +0200

    Added Flavors to the project.

---
+++ lib/main_prod.dart
@@ -0,0 +1,11 @@
+import 'package:codika_flutter_templates/flavors.dart';
+import 'main.dart';
+
+// * Entry point for the prod flavor
+void main() async {
+  // Add environnement dependent code here
+  // ...
+  
+  F.appFlavor = Flavor.prod;
+  await runMainApp();
+}
